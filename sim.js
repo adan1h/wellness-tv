0da4a342f4bc880dc247f92a9857999811763ae8
adan1h <adanst.p61@gmail.com>
2026-09-04 17:09:15 -0400

    Load simulation media pack after seed data.

---
+++ sim.js
@@ -0,0 +1,13 @@
+window.SEED.replay.image = "./media/hero.jpg";
+window.SEED.wall = [
+  { label: "16:9", image: "./media/clip-169.jpg" },
+  { label: "9:16", image: "./media/clip-916.jpg" },
+  { label: "X", image: "./media/clip-x.jpg" }
+];
+window.SEED.gallery = [
+  "./media/still-01.jpg",
+  "./media/still-02.jpg",
+  "./media/still-03.jpg",
+  "./media/still-04.jpg",
+  "./media/still-05.jpg"
+];
