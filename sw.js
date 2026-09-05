587587211c16b33a5660f36728b0b6fca1496cda
adan1h <adanst.p61@gmail.com>
2026-09-04 20:32:40 -0400

    PWA network-first so the theatre is not stuck on an old cache.

--- sw.js
+++ sw.js
@@ -1,6 +1,41 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("wtv-v1").then((c) => c.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"])));
+var CACHE = "wtv-v2";
+var PRE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./layout.css",
+  "./social.css",
+  "./soon.css",
+  "./film.css",
+  "./manifest.json",
+  "./icon.svg",
+  "./data.js",
+  "./broadcast.js",
+  "./app.js",
+  "./social.js",
+  "./player.js",
+  "./soon.js",
+  "./filmroll.js",
+  "./dark.js"
+];
+self.addEventListener("install", function (e) {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(PRE); }));
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+self.addEventListener("fetch", function (e) {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () { return caches.match(e.request); })
+  );
 });
